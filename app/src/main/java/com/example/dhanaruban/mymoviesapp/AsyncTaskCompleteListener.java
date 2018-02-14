243da96b5149f16a564d01742be8ef68b3c10390
dhanaruban <32019096+dhanaruban@users.noreply.github.com>
2018-02-14 20:06:30 +0530

    further

--- app/src/main/java/com/example/dhanaruban/mymoviesapp/AsyncTaskCompleteListener.java
+++ app/src/main/java/com/example/dhanaruban/mymoviesapp/AsyncTaskCompleteListener.java
@@ -1,11 +1,18 @@
 package com.example.dhanaruban.mymoviesapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by thenu on 17-01-2018.
  */
 
 public interface AsyncTaskCompleteListener<T>
 {
+
+
     /**
      * Invoked when the AsyncTask has completed its execution.
      * @param result The resulting object from the AsyncTask.
--- app/src/main/java/com/example/dhanaruban/mymoviesapp/MovieDetailsActivity.java
+++ app/src/main/java/com/example/dhanaruban/mymoviesapp/MovieDetailsActivity.java
@@ -4,7 +4,7 @@ package com.example.dhanaruban.mymoviesapp;
  * Created by dhanaruban on 14/01/18.
  */
 
-import android.app.Fragment;
+
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
@@ -15,6 +15,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,6 +28,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
+import android.support.v4.app.Fragment;
 
 import com.example.dhanaruban.mymoviesapp.utilities.MovieJsonUtils;
 import com.example.dhanaruban.mymoviesapp.utilities.NetworkUtils;
@@ -49,14 +51,10 @@ public class MovieDetailsActivity extends Fragment implements AsyncTaskCompleteL
 
     private LayoutInflater mLayoutInflater;
     private View rootView;
-    private View tvOriginalTitle;
-
-
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
 
         setHasOptionsMenu(true);
         mLayoutInflater = inflater;
@@ -64,35 +62,92 @@ public class MovieDetailsActivity extends Fragment implements AsyncTaskCompleteL
         Bundle arguments = getArguments();
         Intent intent = getActivity().getIntent();
 
-        if(arguments != null || intent != null && intent.hasExtra("movies_details")){
+        if (arguments != null || intent != null && intent.hasExtra("movies_details")) {
 
             rootView = mLayoutInflater.inflate(R.layout.activity_movie_details, container, false);
             if (arguments != null) {
-                movie = (Movie)getArguments().getParcelable("movies_details");
-            }else{
-                movie = (Movie)intent.getParcelableExtra("movies_details");
+                movie = (Movie) getArguments().getParcelable("movies_details");
+            } else {
+                movie = (Movie) intent.getParcelableExtra("movies_details");
             }
             // display the main movie info
             DisplayInfo();
 
 
-        }else{
+        } else {
             rootView = mLayoutInflater.inflate(R.layout.placeholder,
                     container, false);
         }
 
         return rootView;
     }
+    private void addToFavorites() {
+
+        Uri uri = MovieContract.MovieEntry.CONTENT_URI;
+        ContentResolver resolver = getActivity().getContentResolver();
+        ContentValues values = new ContentValues();
+        values.clear();
+
+        values.put(MovieContract.MovieEntry.MOVIE_ID, movie.getId());
+        values.put(MovieContract.MovieEntry.MOVIE_BACKDROP_URI, movie.getTitle());
+        values.put(MovieContract.MovieEntry.MOVIE_TITLE, movie.getTitle());
+        values.put(MovieContract.MovieEntry.MOVIE_POSTER, movie.getPoster_path());
+        values.put(MovieContract.MovieEntry.MOVIE_OVERVIEW, movie.getOverview());
+        values.put(MovieContract.MovieEntry.MOVIE_VOTE_AVERAGE, movie.getVote_average());
+        values.put(MovieContract.MovieEntry.MOVIE_RELEASE_DATE, movie.getRelease_date());
+
+
+
+        Uri check = resolver.insert(uri, values);
+    }
+    private void deleteFromFavorites() {
+
+        Uri uri = MovieContract.MovieEntry.CONTENT_URI;
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        long noDeleted = resolver.delete(uri,
+                MovieContract.MovieEntry.MOVIE_ID + " = ? ",
+                new String[]{ movie.getId() + "" });
+
+    }
+    private boolean checkFavorites() {
+
+        Uri uri = MovieContract.MovieEntry.buildMovieUri(movie.getId());
+        ContentResolver resolver = getActivity().getContentResolver();
+        Cursor cursor = null;
+
+        try {
+
+            cursor = resolver.query(uri, null, null, null, null);
+            if (cursor.moveToFirst())
+                return true;
+
+        } finally {
+
+            if(cursor != null)
+                cursor.close();
+
+        }
+
+        return false;
+    }
 
     private void DisplayInfo() {
 
-        TextView tvOriginalTitle = (TextView)rootView.findViewById(R.id.textview_original_title);
-        ImageView ivPoster = (ImageView)rootView.findViewById(R.id.imageview_poster);
-        TextView tvOverView = (TextView)rootView.findViewById(R.id.textview_overview);
+        TextView tvOriginalTitle = (TextView) rootView.findViewById(R.id.textview_original_title);
+        ImageView ivPoster = (ImageView) rootView.findViewById(R.id.imageview_poster);
+        TextView tvOverView = (TextView) rootView.findViewById(R.id.textview_overview);
         ImageButton addToFav = (ImageButton) rootView.findViewById(R.id.add_to_fav_view);
         toggleFavorites();
-        TextView tvVoteAverage = (TextView)rootView.findViewById(R.id.textview_vote_average);
-        TextView tvReleaseDate = (TextView)rootView.findViewById(R.id.textview_release_date);
+        TextView tvVoteAverage = (TextView) rootView.findViewById(R.id.textview_vote_average);
+        TextView tvReleaseDate = (TextView) rootView.findViewById(R.id.textview_release_date);
+        Intent intent = getActivity().getIntent();
+        movie =intent.getParcelableExtra(getString(R.string.parcel_movie));
+        tvOriginalTitle.setText(movie.getTitle());
+        Picasso.with(getActivity()).load(movie.getPoster_path()).into(ivPoster);
+        tvReleaseDate.setText(movie.getRelease_date());
+        tvVoteAverage.setText(movie.getVote_average() + "/10");
+        tvOverView.setText(movie.getOverview());
         addToFav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -105,45 +160,8 @@ public class MovieDetailsActivity extends Fragment implements AsyncTaskCompleteL
                 toggleFavorites();
             }
         });
-
-
-        Intent intent =getActivity().getIntent();
-        movie = intent.getParcelableExtra(getString(R.string.parcel_movie));
-
-        tvOriginalTitle.setText(movie.getOriginal_title());
-
-        Picasso.with(this)
-                .load(BASE_PATH + movie.getPoster_path())
-                .into(ivPoster);
-
-        String overView = movie.getOverview();
-        if (overView == null) {
-            tvOverView.setTypeface(null, Typeface.ITALIC);
-            overView = getResources().getString(R.string.no_summary_found);
-        }
-        tvOverView.setText(overView);
-        tvVoteAverage.setText(Double.toString(movie.getVote_average()));
-
-        // First get the release date from the object - to be used if something goes wrong with
-        // getting localized release date (catch).
-        String releaseDate = movie.getRelease_date();
-//        if(releaseDate != null) {
-//            try {
-//                releaseDate = DateTimeHelper.getLocalizedDate(this,
-//                        releaseDate, movie.getDateFormat());
-//            } catch (ParseException e) {
-//                Log.e(LOG_TAG, "Error with parsing movie release date", e);
-//            }
-//        } else {
-//            tvReleaseDate.setTypeface(null, Typeface.ITALIC);
-//            releaseDate = getResources().getString(R.string.no_release_date_found);
-//        }
-        tvReleaseDate.setText(releaseDate);
-
-        //vvTrailer.setVideoPath(BASE_PATH + "/movie/" + movie.getId() + "/videos");
-
-
     }
+
 
     private void toggleFavorites() {
         boolean inFavorites = checkFavorites();
@@ -201,55 +219,7 @@ public class MovieDetailsActivity extends Fragment implements AsyncTaskCompleteL
 //
 //        }
    }
-    private void addToFavorites() {
-
-        Uri uri = MovieContract.MovieEntry.CONTENT_URI;
-        ContentResolver resolver = getActivity().getContentResolver();
-        ContentValues values = new ContentValues();
-        values.clear();
-
-        values.put(MovieContract.MovieEntry.MOVIE_ID, movie.getId());
-        values.put(MovieContract.MovieEntry.MOVIE_BACKDROP_URI, movie.getTitle());
-        values.put(MovieContract.MovieEntry.MOVIE_TITLE, movie.getTitle());
-        values.put(MovieContract.MovieEntry.MOVIE_POSTER, movie.getPoster_path());
-        values.put(MovieContract.MovieEntry.MOVIE_OVERVIEW, movie.getOverview());
-        values.put(MovieContract.MovieEntry.MOVIE_VOTE_AVERAGE, movie.getVote_average());
-        values.put(MovieContract.MovieEntry.MOVIE_RELEASE_DATE, movie.getRelease_date());
 
 
-
-        Uri check = resolver.insert(uri, values);
-    }
-    private void deleteFromFavorites() {
-
-        Uri uri = MovieContract.MovieEntry.CONTENT_URI;
-        ContentResolver resolver = getActivity().getContentResolver();
-
-        long noDeleted = resolver.delete(uri,
-                MovieContract.MovieEntry.MOVIE_ID + " = ? ",
-                new String[]{ movie.getId() + "" });
-
-    }
-    private boolean checkFavorites() {
-
-        Uri uri = MovieContract.MovieEntry.buildMovieUri(movie.getId());
-        ContentResolver resolver = getActivity().getContentResolver();
-        Cursor cursor = null;
-
-        try {
-
-            cursor = resolver.query(uri, null, null, null, null);
-            if (cursor.moveToFirst())
-                return true;
-
-        } finally {
-
-            if(cursor != null)
-                cursor.close();
-
-        }
-
-        return false;
-    }
 
 }
